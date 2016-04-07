4a815a347b72106796e9d40966eddd771c241c67
freek <freek@spatie.be>
2016-04-07 13:51:55 +0100

    wip

---
+++ src/Exceptions/Untranslatable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Spatie\Translatable\Exceptions;
+
+use Exception;
+use Spatie\Translatable\Translatable;
+
+class Untranslatable extends Exception
+{
+    public static function fieldIsNotTranslatable(string $fieldName, Translatable $model) : static
+    {
+        $translatableFieldNames = implode(',', $model->getTranslatableFields());
+
+        return new static("Cannot translated field `{$fieldName}` as it does not one of the translatable fieldnames: `$translatableFieldNames`");
+    }
+}
--- src/HasTranslations.php
+++ src/HasTranslations.php
@@ -2,27 +2,63 @@
 
 namespace Spatie\Translatable;
 
+use Spatie\Translatable\Exceptions\Untranslatable;
+
 trait HasTranslations
 {
-    public function translate(string $fieldName, string $locale = '') : string
+    /**
+     * @param string $fieldName
+     *
+     * @return mixed
+     */
+    public function getAttribute(string $fieldName)
     {
-        return $this->getTranslation($fieldName, $locale);
+        if (!$this->isTranslatableField($fieldName)) {
+            return parent::getAttribute($fieldName);
+        }
+
+        return $this->getTranslation($fieldName, app()->getLocale());
     }
 
-    public function getTranslation(string $fieldName, string $locale) : string
+    public function translate(string $fieldName, string $locale = '', string $default = '') : string
     {
+        return $this->getTranslation($fieldName, $locale, $default);
+    }
 
+    public function getTranslation(string $fieldName, string $locale, string $default = '') : string
+    {
+        $this->guardAgainstUntranslatableFieldName($fieldName);
+
+        return $this->$fieldName[$locale] ?? $default;
     }
 
     public function setTranslation(string $fieldName, string $locale, string $value)
     {
+        $this->guardAgainstUntranslatableFieldName($fieldName);
 
+        $this->$fieldName[$locale] = $value;
+
+        return $this;
     }
 
     public function setTranslations(string $fieldName, array $values)
     {
-        foreach($values as $locale => $value) {
+        $this->guardAgainstUntranslatableFieldName($fieldName);
+
+        foreach ($values as $locale => $value) {
             $this->setTranslation($fieldName, $locale, $value);
+        }
+    }
+
+    protected function isTranslatableField(string $fieldName) : bool
+    {
+        return in_array($fieldName, $this->getTranslatableFields());
+    }
+
+    protected function guardAgainstUntranslatableFieldName(string $fieldName)
+    {
+        if (!$this->isTranslatableField($fieldName)) {
+            throw Untranslatable::fieldIsNotTranslatable($fieldName, $this);
         }
     }
 }
