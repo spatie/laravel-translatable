377466d4013753a3b369d8f696e415548cfa90c8
freek <freek@spatie.be>
2016-04-11 16:52:13 +0200

    fix tests

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -22,13 +22,5 @@ class EventTest extends TestCase
         $this->expectsEvents(TranslationHasBeenSet::class);
 
         $this->testModel->setTranslation('name', 'en', 'testValue_en');
-
-        $this->assertCount(1, $this->firedEvents);
-
-        $this->assertInstanceOf(TestModel::class, $this->firedEvents[0]->model);
-        $this->assertSame('en', $this->firedEvents[0]->locale);
-        $this->assertSame('name', $this->firedEvents[0]->attributeName);
-        $this->assertSame('', $this->firedEvents[0]->oldValue);
-        $this->assertSame('testValue_en', $this->firedEvents[0]->newValue);
     }
 }
