35114b13928215a3d4341b63c78b8baa042293d2
Florian Heller <6016611+flofloflo@users.noreply.github.com>
2017-09-21 14:54:31 +0200

    Added test case for translations without specified locale

--- tests/TranslatableTest.php
+++ tests/TranslatableTest.php
@@ -238,6 +238,28 @@ class TranslatableTest extends TestCase
     }
 
     /** @test */
+    public function it_can_set_translations_for_default_language()
+    {
+        $model = TestModel::create([
+            'name' => [
+                'en' => 'testValue_en',
+                'fr' => 'testValue_fr',
+            ],
+        ]);
+
+        app()->setLocale('en');
+
+        $model->name = 'updated_en';
+        $this->assertEquals('updated_en', $model->name);
+        $this->assertEquals('testValue_fr', $model->getTranslation('name', 'fr'));
+
+        app()->setLocale('fr');
+        $model->name = 'updated_fr';
+        $this->assertEquals('updated_fr', $model->name);
+        $this->assertEquals('updated_en', $model->getTranslation('name', 'en'));
+    }
+
+    /** @test */
     public function it_can_set_multiple_translations_at_once()
     {
         $translations = ['nl' => 'hallo', 'en' => 'hello', 'kh' => 'សួរស្តី'];
