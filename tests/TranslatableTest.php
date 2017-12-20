b754f7058f5c52e3f3641b82d1743bb3ffd96c9e
Freek Van der Herten <freek@spatie.be>
2017-12-20 19:23:39 +0000

    Apply fixes from StyleCI

--- tests/TranslatableTest.php
+++ tests/TranslatableTest.php
@@ -307,7 +307,7 @@ class TranslatableTest extends TestCase
         $translations = [
             'nl' => 'hallo',
             'en' => 'hello',
-            'kh' => 'សួរស្តី'
+            'kh' => 'សួរស្តី',
         ];
 
         $testModel->setTranslations('name', $translations);
@@ -317,7 +317,7 @@ class TranslatableTest extends TestCase
         $expected = [
             'nl' => 'I just mutated hallo',
             'en' => 'I just mutated hello',
-            'kh' => 'I just mutated សួរស្តី'
+            'kh' => 'I just mutated សួរស្តី',
         ];
 
         $this->assertEquals($expected, $testModel->getTranslations('name'));
