627d5ffd0a1d1db038d7029c2e09ce7e56319145
Yashar Imanlou <yashari@gmail.com>
2020-04-28 06:34:19 +0000

    Add field_with_mutator to test scenario

--- tests/TestModel.php
+++ tests/TestModel.php
@@ -14,5 +14,10 @@ class TestModel extends Model
     protected $guarded = [];
     public $timestamps = false;
 
-    public $translatable = ['name', 'other_field'];
+    public $translatable = ['name', 'other_field', 'field_with_mutator'];
+
+    public function setFieldWithMutatorAttribute($value)
+    {
+        $this->attributes['field_with_mutator'] = $value;
+    }
 }
