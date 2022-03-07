ea5378d49b5720882bc8fb34ead7fbd233b7c0cb
freekmurze <freekmurze@users.noreply.github.com>
2022-03-07 15:17:38 +0000

    Fix styling

--- tests/TranslatableTest.php
+++ tests/TranslatableTest.php
@@ -397,7 +397,8 @@ it('can use accessors on translated attributes', function () {
 
 it('can use mutators on translated attributes', function () {
     $testModel = new class () extends TestModel {
-        public function setNameAttribute($value) {
+        public function setNameAttribute($value)
+        {
             $this->attributes['name'] = "I just mutated {$value}";
         }
     };
@@ -454,7 +455,8 @@ it('can check if an attribute has translation', function () {
 
 it('can correctly set a field when a mutator is defined', function () {
     $testModel = (new class () extends TestModel {
-        public function setNameAttribute($value) {
+        public function setNameAttribute($value)
+        {
             $this->attributes['name'] = "I just mutated {$value}";
         }
     });
@@ -467,7 +469,8 @@ it('can correctly set a field when a mutator is defined', function () {
 
 it('can set multiple translations when a mutator is defined', function () {
     $testModel = (new class () extends TestModel {
-        public function setNameAttribute($value) {
+        public function setNameAttribute($value)
+        {
             $this->attributes['name'] = "I just mutated {$value}";
         }
     });
@@ -506,7 +509,8 @@ it('can set multiple translations on field when a mutator is defined', function 
 
 it('can translate a field based on the translations of another one', function () {
     $testModel = (new class () extends TestModel {
-        public function setOtherFieldAttribute($value, $locale = 'en') {
+        public function setOtherFieldAttribute($value, $locale = 'en')
+        {
             $this->attributes['other_field'] = $value.' '.$this->getTranslation('name', $locale);
         }
     });
@@ -533,7 +537,8 @@ it('can translate a field based on the translations of another one', function ()
 
 it('handle null value from database', function () {
     $testModel = (new class () extends TestModel {
-        public function setAttributesExternally(array $attributes) {
+        public function setAttributesExternally(array $attributes)
+        {
             $this->attributes = $attributes;
         }
     });
