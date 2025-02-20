87167fdfa06eeef7764b3e5b52cc665667198c11
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-02-20 15:50:33 +0000

    Fix styling

--- tests/TestSupport/TestModelWithFallbackLocale.php
+++ tests/TestSupport/TestModelWithFallbackLocale.php
@@ -14,6 +14,7 @@ class TestModelWithFallbackLocale extends Model
     protected $table = 'test_models';
 
     protected $guarded = [];
+
     public $timestamps = false;
 
     public $translatable = ['name', 'other_field', 'field_with_mutator'];
